ce4fe271f117d8f6dec5ec318695ced8c62d4d33
Jalalabadi <jalalabadijk@gmail.com>
2024-06-12 11:34:35 +0600

    up06/12/2024-2time

--- routeHandelar/task.js
+++ routeHandelar/task.js
@@ -50,91 +50,81 @@ const upload = multer({
 
 ////New Task Submit For Worker
 router.post('/add', upload.fields([{ name: 'proof1' }, { name: 'proof2' }, 
-                    { name: 'proof3' }, { name: 'proof4' }]), authCheck, async(req, res)=>{
+  { name: 'proof3' }, { name: 'proof4' }]), authCheck, async (req, res) => {
   try {
-    if(req.userData?.userName){
-      const user = await User.findOne({ where: { userName: req.userData?.userName }});
-      const job = await Job.findOne({ where: { id: req.body.jobID, status: "approved"}});
-      const completedTasks = await Task.findAll({ where: { userName: req.userData.userName } });
-      const completedTaskIds = completedTasks.map(task => task.jobID);
-      if(completedTaskIds.includes(parseFloat(req.body.jobID))) {
-        res.status(400).send('Already Done');
-        }
-      else if(!user&&!job&&((job.taskDone-job.taskCancel)>job.workersNeed)){
-        res.status(500).send('Job Not found');
-      }
-      else if(job.workersNeed<=(job.taskDone-job.taskCancel)){
-        res.status(500).send('Job Already Complete');
-      }
-    else if(job.ratingType==="system_verify"|| job.ratingType==="system_rate"){
-  const jobRemind = job.workersNeed-(job.taskDone-job.taskCancel);
-  const employer = await User.findOne({ where: { userName: job.userName }});
-  const hash = CryptoJS.SHA256(job.id+user.id+employer.SecretKey);
-   const vcode ='lw-' + hash.toString(CryptoJS.enc.Hex);
-                   /////////////////////////Vcode Verify
-                   if(req.body.proof1===vcode){
-                    await user.update({
-                      pending: job.ratingType==="system_verify"?parseFloat(user.pending)+parseFloat(job.taskCost):user.pending,
-                      earned: job.ratingType==="system_rate"?parseFloat(user.earned)+parseFloat(job.taskCost):user.earned,
-                      tasksDone: user.tasksDone+1
-                    });
-                    await job.update({
-                      taskDone: job.taskDone+1,
-                      status: jobRemind<=1?"completed":job.status
-                    });
-              
-                  await Task.create({
-                    proof1: req.files.proof1?req.files.proof1[0].filename:req.body.proof1, 
-                    proof2:req.files.proof2?req.files.proof2[0].filename:req.body.proof2, 
-                    proof3:req.files.proof3?req.files.proof3[0].filename:req.body.proof3, 
-                    proof4:req.files.proof4?req.files.proof4[0].filename:req.body.proof4, 
-                    status: job.ratingType==="system_rate"?'approved':'pending',
-                    jobID: req.body.jobID,
-                    ip: user.ip,
-                    country: user.country,
-                    userName: req.userData?.userName
-                    
-                  });
-                  res.status(200).json("Success");
-                   }
-                   else{
-                    res.status(500).send('Invalid Vcode');
-                   }
+    const { userName } = req.userData || {};
+    if (!userName) {
+      return res.status(500).send('User Not Found');
     }
-    else{
-      const jobRemind = job.workersNeed-(job.taskDone-job.taskCancel);
+
+    const user = await User.findOne({ where: { userName } });
+    const job = await Job.findOne({ where: { id: req.body.jobID, status: "approved" } });
+
+    if (!user || !job || (job.taskDone - job.taskCancel) > job.workersNeed) {
+      return res.status(500).send('Job Not found');
+    }
+
+    if (job.workersNeed <= (job.taskDone - job.taskCancel)) {
+      return res.status(500).send('Job Not available at the moment');
+    }
+
+    const completedTasks = await Task.findAll({ where: { userName } });
+    const completedTaskIds = completedTasks.map(task => task.jobID);
+
+    if (completedTaskIds.includes(parseFloat(req.body.jobID))) {
+      return res.status(400).send('Already Done');
+    }
+
+    const jobRemind = job.workersNeed - (job.taskDone - job.taskCancel);
+    let taskStatus = 'pending';
+
+    if (["system_verify", "system_rate"].includes(job.ratingType)) {
+      const employer = await User.findOne({ where: { userName: job.userName } });
+      const hash = CryptoJS.SHA256(job.id+user.id+employer.SecretKey);
+      const vcode = 'lw-' + hash.toString(CryptoJS.enc.Hex);
+
+      if (req.body.proof1 !== vcode) {
+        return res.status(500).send('Invalid Vcode');
+      }
+
+      const updatedField = job.ratingType === "system_verify" ? 'pending' : 'earned';
       await user.update({
-        pending: parseFloat(user.pending)+parseFloat(job.taskCost),
-        tasksDone: user.tasksDone+1
-      });
-      await job.update({
-        taskDone: job.taskDone+1,
-        status: jobRemind<=1?"completed":job.status
+        [updatedField]: parseFloat(user[updatedField]) + parseFloat(job.taskCost),
+        tasksDone: user.tasksDone + 1
       });
 
+      taskStatus = job.ratingType === "system_rate" ? 'approved' : 'pending';
+    } else {
+      await user.update({
+        pending: parseFloat(user.pending) + parseFloat(job.taskCost),
+        tasksDone: user.tasksDone + 1
+      });
+    }
+
+    await job.update({
+      taskDone: job.taskDone + 1,
+      status: jobRemind <= 1 ? "completed" : job.status
+    });
+
     await Task.create({
-      proof1: req.files.proof1?req.files.proof1[0].filename:req.body.proof1, 
-      proof2:req.files.proof2?req.files.proof2[0].filename:req.body.proof2, 
-      proof3:req.files.proof3?req.files.proof3[0].filename:req.body.proof3, 
-      proof4:req.files.proof4?req.files.proof4[0].filename:req.body.proof4, 
-      status: 'pending',
+      proof1: req.files.proof1 ? req.files.proof1[0].filename : req.body.proof1,
+      proof2: req.files.proof2 ? req.files.proof2[0].filename : req.body.proof2,
+      proof3: req.files.proof3 ? req.files.proof3[0].filename : req.body.proof3,
+      proof4: req.files.proof4 ? req.files.proof4[0].filename : req.body.proof4,
+      status: taskStatus,
       jobID: req.body.jobID,
       ip: user.ip,
       country: user.country,
-      userName: req.userData?.userName
-      
+      userName
     });
+
     res.status(200).json("Success");
-  }
-  }
-  else{
-    res.status(500).send('User Not Found');
-  }
   } catch (error) {
-    console.error('Failed to retrieve last seen timestamp:', error);
+    console.error('Failed to process task submission:', error);
     res.status(500).send('Internal server error');
   }
 });
+
 
 ////Hide Task Submit For Worker
 router.post('/job-hide',  authCheck, async(req, res)=>{
@@ -438,8 +428,8 @@ router.post('/revise-submit',  upload.fields([{ name: 'proof1' }, { name: 'proof
       proof3:req.files.proof3?req.files.proof3[0].filename:req.body.proof3, 
       proof4:req.files.proof4?req.files.proof4[0].filename:req.body.proof4, 
       status: 'pending',
-      ip: "01255.5555.3644",
-      country: "Bangladesh"
+      ip: req.userData?.ip,
+      country: req.userData?.country
         });
         res.status(200).json("Success");
       }
